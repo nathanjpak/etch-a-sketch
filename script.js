4b29b31910c4ee87d3e752f7bccb9a886ffe7d31
nathanjpak <nathan.j.pak@gmail.com>
2022-06-29 18:54:25 -0700

    allow user to edit number of rows in grid

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const container = document.getElementById("container");
+const numberButton = document.getElementById("numberButton")
 
-for (i=0; i<16; i++) {
-    let newSquare = document.createElement("div");
-    newSquare.className = "square";
-    container.appendChild(newSquare);
+numberButton.addEventListener("click", function() {
+    let rows = prompt("How many rows? (min: 1, max: 50)", 10);
+    if (rows >=1 && rows <= 50) {
+        clearGrid();
+        loadGrid(rows);
+    } else {
+        alert("Please enter a valid number.");
+    }
+})
+
+function loadGrid(rows) {
+    rows = Math.floor(rows);
+    container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    num = rows*rows;
+    loadSquares(num);
 }
 
-const squares = Array.from(document.getElementsByClassName("square"));
+function loadSquares(num) {
+    for (i=0; i<num; i++) {
+        let newSquare = document.createElement("div");
+        newSquare.className = "square";
+        container.appendChild(newSquare);
+    }
+    const squares = Array.from(document.getElementsByClassName("square"));
+    squares.forEach(square => square.addEventListener("mouseover", changeColor));
+}
+
+function clearGrid() {
+    while (container.firstElementChild) {
+        let square = container.firstElementChild;
+        container.removeChild(square);
+    }
+}
 
 function changeColor(e) {
-    console.log(e.target);
     e.target.style.backgroundColor = "black";
 }
 
-squares.forEach(square => square.addEventListener("mouseover", changeColor));
+loadGrid(4.2);
